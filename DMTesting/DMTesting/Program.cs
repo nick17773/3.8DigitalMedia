dcf8e8d8efe2246ba20db794918609bd8f71b285
nick17773 <n.a.russell2015@gmail.com>
2021-05-18 09:52:31 +1200

    Proof of Concept

--- DMTesting/DMTesting/Program.cs
+++ DMTesting/DMTesting/Program.cs
@@ -5,49 +5,88 @@ namespace DMTesting
 {
     class Program
     {
+        
+
         static void Main(string[] args)
         {
-            Dictionary<string, Genre> genreDictionary = new Dictionary<string, Genre>
+            Dictionary<string, Genre> genreDictionary = new()
             {
                 { "Heavy", new Genre("Heavy Metal", BPM.Medium, "Standard", TimeSignature.Static) },
                 { "Thrash", new Genre("Thrash Metal", BPM.Fast, "Standard", TimeSignature.Static) }
             };
+            string genreIndicator = "Your genre is:";
 
-            
 
-            
-            var genreIndicator = "Your genre is:";
+
+
             Console.WriteLine("Tuning Input");
             var TuningInput = Console.ReadLine();
-            
-            if(TuningInput == "Standard")
+            Console.WriteLine("BPM Input");
+            var BPMInput = Console.ReadLine();
+            Console.WriteLine("Time Singature Input");
+            var TimeSigInput = Console.ReadLine();
+            if (TuningInput == "Standard")
             {
                 
 
-                Console.WriteLine("BPM Input");
-                var BPMInput = Console.ReadLine();
+                //Console.WriteLine("BPM Input");
+                //var BPMInput = Console.ReadLine();
                 if(BPMInput == Convert.ToString(BPM.Fast))
                 {
-                    Console.WriteLine(genreIndicator);
-                    Console.WriteLine(genreDictionary["Thrash"].name);
-                    Console.WriteLine($"BPM: {genreDictionary["Thrash"].bpm}");
-                    Console.WriteLine($"Tuning: {genreDictionary["Thrash"].tuning}");
-                    Console.WriteLine($"Time Signature: {genreDictionary["Thrash"].timeSignature}");
+                    //Console.WriteLine(genreIndicator);
+                    //Console.WriteLine(genreDictionary["Thrash"].name);
+                    //Console.WriteLine($"BPM: {genreDictionary["Thrash"].bpm}");
+                    //Console.WriteLine($"Tuning: {genreDictionary["Thrash"].tuning}");
+                    //Console.WriteLine($"Time Signature: {genreDictionary["Thrash"].timeSignature}");
+                    if (TimeSigInput == Convert.ToString(TimeSignature.Static))
+                    {
+                        string genreKey = "Thrash";
+                        GenreDisplay(genreKey, genreDictionary, genreIndicator);
+                        //Console.WriteLine(genreIndicator);
+                        //Console.WriteLine(genreDictionary["Thrash"].name);
+                        //Console.WriteLine($"BPM: {genreDictionary["Thrash"].bpm}");
+                        //Console.WriteLine($"Tuning: {genreDictionary["Thrash"].tuning}");
+                        //Console.WriteLine($"Time Signature: {genreDictionary["Thrash"].timeSignature}");
+                    }
+
                 }
                 else if (BPMInput == Convert.ToString(BPM.Medium))
                 {
-                    Console.WriteLine(genreIndicator);
-                    Console.WriteLine(genreDictionary["Heavy"].name);
-                    Console.WriteLine($"BPM: {genreDictionary["Heavy"].bpm}");
-                    Console.WriteLine($"Tuning: {genreDictionary["Heavy"].tuning}");
-                    Console.WriteLine($"Time Signature: {genreDictionary["Heavy"].timeSignature}");
+                    //Console.WriteLine(genreIndicator);
+                    //Console.WriteLine(genreDictionary["Heavy"].name);
+                    //Console.WriteLine($"BPM: {genreDictionary["Heavy"].bpm}");
+                    //Console.WriteLine($"Tuning: {genreDictionary["Heavy"].tuning}");
+                    //Console.WriteLine($"Time Signature: {genreDictionary["Heavy"].timeSignature}");
+                    
+                    if (TimeSigInput == Convert.ToString(TimeSignature.Static))
+                    {
+                        string genreKey = "Heavy";
+                        GenreDisplay(genreKey, genreDictionary, genreIndicator);
+                        //Console.WriteLine(genreIndicator);
+                        //Console.WriteLine(genreDictionary[genreKey].name);
+                        //Console.WriteLine($"BPM: {genreDictionary[genreKey].bpm}");
+                        //Console.WriteLine($"Tuning: {genreDictionary[genreKey].tuning}");
+                        //Console.WriteLine($"Time Signature: {genreDictionary[genreKey].timeSignature}");
+                    }
                 }
+
             }
 
             //Debug.WriteLine(genreDictionary["Lol"]);
+            static void GenreDisplay(string genreKey, Dictionary<string, Genre> genreDictionary, string genreIndicator)
+            {
+                Console.WriteLine(genreIndicator);
+                Console.WriteLine(genreDictionary[genreKey].name);
+                Console.WriteLine($"BPM: {genreDictionary[genreKey].bpm}");
+                Console.WriteLine($"Tuning: {genreDictionary[genreKey].tuning}");
+                Console.WriteLine($"Time Signature: {genreDictionary[genreKey].timeSignature}");
+            }
 
         }
     }
+
+
+
     public enum BPM
     {
         Slow,
